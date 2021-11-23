cded2ea319662eda6d368d09eb03e39713b48906
Yiwei Zhuang <zhuangyiweiwei@126.com>
2021-11-23 12:08:59 -0600

    ADD: prod config

--- dao/connections.js
+++ dao/connections.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const mysql_client = mysql.createPool({
     connectionLimit: 10,
     host: "localhost",
-    database: "CSE503S_final",
+    database: "draw_guess",
     user: "yiwei_z",
     password: "hehe13"
 });
